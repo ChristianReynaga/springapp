228b3ce3a48793673759cc1ab976e8db4da0ff31
ChristianReynaga <christian.reynaga.cr@gmail.com>
2022-08-15 18:03:14 -0400

    - Se agregó ItemsController

--- src/main/java/dev/chrislancer/springapp/rest/HelloController.java
+++ src/main/java/dev/chrislancer/springapp/rest/HelloController.java
@@ -1,22 +1,16 @@
 package dev.chrislancer.springapp.rest;
 
-import dev.chrislancer.springapp.security.AuthRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("")
+@Slf4j
 public class HelloController {
-
-    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @GetMapping("/user")
     public ResponseEntity<?> helloUser() {
@@ -34,9 +28,9 @@ public class HelloController {
 
     private void logAuthentication() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        logger.info("USER: {}", auth.getPrincipal());
-        logger.info("AUTHORITIES: {}", auth.getAuthorities());
-        logger.info("IS AUTHENTICATED: {}", auth.isAuthenticated());
+        log.info("USER: {}", auth.getPrincipal());
+        log.info("AUTHORITIES: {}", auth.getAuthorities());
+        log.info("IS AUTHENTICATED: {}", auth.isAuthenticated());
     }
 
 }
--- src/main/java/dev/chrislancer/springapp/rest/RegistrationController.java
+++ src/main/java/dev/chrislancer/springapp/rest/RegistrationController.java
@@ -2,13 +2,18 @@ package dev.chrislancer.springapp.rest;
 
 import dev.chrislancer.springapp.security.User;
 import dev.chrislancer.springapp.security.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class RegistrationController {
 
     @Autowired
@@ -17,10 +22,14 @@ public class RegistrationController {
     PasswordEncoder encoder;
 
     @PostMapping("/register")
-    public void register(@RequestBody User user) {
+    public void register(@AuthenticationPrincipal UserDetails userDetails, @RequestBody User user) {
         // input validation omitted for brevity
-        user.setPassword(encoder.encode(user.getPassword()));
-        userRepo.save(user);
+        if (userDetails.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))){
+            user.setPassword(encoder.encode(user.getPassword()));
+            userRepo.save(user);
+        }else {
+            log.info("You need ROLE_ADMIN");
+        }
     }
 
 }
